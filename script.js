9f5e88c2d93f159d15a170f43eb0947c4c7124af
Mohammed Badat <moebadat16@gmail.com>
2023-04-11 11:25:14 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function values(){
+let fname=document.getElementById('fname').value;
+let lname=document.getElementById('lname').value;
+let email=document.getElementById('email').value;
+console.log('First name:', fname)
+console.log('Last Name:',lname )
+console.log('Email:',email )
+
+}
